e51d65ab6fae5c9eb0ac75d68efb6e252ce8cf47
Kate-Iva <152105481+Kate-Iva@users.noreply.github.com>
2025-05-28 01:03:14 +0100

    Mobile Menu update

--- js/script.js
+++ js/script.js
@@ -41,4 +41,45 @@ document.addEventListener('DOMContentLoaded', () => {
         });
       }
     });
+});
+
+
+    //Mibile menu
+    document.addEventListener('DOMContentLoaded', () => {
+    const mobMenu = document.querySelector('.header-menu-mob');
+    const openBtn = document.querySelector('.menu-open-btn'); 
+    const closeBtn = document.querySelector('.mob-close-btn');
+   
+    if (openBtn) {
+      openBtn.addEventListener('click', () => {
+        mobMenu.classList.add('is-open');
+        mobMenu.classList.remove('is-close');
+      });
+    }
+    
+    if (closeBtn) {
+      closeBtn.addEventListener('click', () => {
+        mobMenu.classList.remove('is-open');
+        mobMenu.classList.add('is-close');
+      });
+    }
+   
+    const mobLinks = document.querySelectorAll('.mob-menu-link');
+    mobLinks.forEach(link => {
+      link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const targetId = link.getAttribute('href').substring(1);
+        const targetElem = document.getElementById(targetId);
+        if (targetElem) {
+          targetElem.scrollIntoView({ behavior: 'smooth' });
+        }
+        
+        mobMenu.classList.remove('is-open');
+        mobMenu.classList.add('is-close');
+        
+        document.querySelectorAll('.mob-menu-link').forEach(l => l.classList.remove('current'));
+        link.classList.add('current');
+      });
+    });
+
   });
